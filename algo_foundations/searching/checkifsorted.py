124715263901ef320c821a7ea25824d8f5dea346
Toon Bosmans <toon.j.bosmans@gmail.com>
2021-01-25 12:08:32 -0600

    check if list is sorted

---
+++ algo_foundations/searching/checkifsorted.py
@@ -0,0 +1,7 @@
+#check if a list is sorted
+
+def issorted(itemlist):
+    #you can do this with a for loop but we'll use a python comprehension here
+    #in JS similar array method: .every 
+    return all(itemlist[i] <= itemlist[i+1] for i in range(len(itemlist) -1))
+
